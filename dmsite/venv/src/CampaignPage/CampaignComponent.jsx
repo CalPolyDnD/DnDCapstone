2f5fb9f66e50b135cb5fed0780ca09fbfa6f629c
Dustyn Zierman-Felix <dziermanfelix@gmail.com>
2019-01-10 10:15:19 -0800

    Campaign page init

---
+++ dmsite/venv/src/CampaignPage/CampaignComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Container, ListGroup, ListGroupItem } from 'reactstrap';
+
+class CampaignComponent extends React.Component {
+
+  render() {
+    return (
+        <h1> Hey </h1>
+    );
+  }
+}
+
+export default CampaignComponent;
