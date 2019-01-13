c18ece254e06ece7462719bf481a2e535050c33d
Liang Cheung <lzcheung@calpoly.edu>
2019-01-13 00:30:34 -0800

    add dropdown for filters

--- dmsite/venv/src/Header/Header.jsx
+++ dmsite/venv/src/Header/Header.jsx
@@ -3,7 +3,11 @@ import { withRouter } from 'react-router-dom';
 
 import {
   Button,
+  DropdownItem,
+  DropdownMenu,
+  DropdownToggle,
   Input,
+  InputGroupButtonDropdown,
   Form,
   Row,
 } from 'reactstrap';
@@ -11,13 +15,39 @@ import {
 import Logo from '../images/DnDLogo2.png';
 import './style.css';
 
+const SEARCH_FIELDS = {
+  name: 'Name',
+  classification: 'Classification',
+  attribute: 'Attribute',
+};
+
 class Header extends React.Component {
   constructor(props) {
     super(props);
+
     this.state = {
+      dropdownOpen: false,
       searchbarText: '',
-      isLoggedIn: false,
+      searchFilterOrder: [
+        SEARCH_FIELDS.name,
+        SEARCH_FIELDS.classification,
+        SEARCH_FIELDS.attribute,
+      ],
     };
+    this.toggleDropDown = this.toggleDropDown.bind(this);
+  }
+
+  switchFilter(index) {
+    const newOrder = this.state.searchFilterOrder.slice();
+    newOrder[0] = this.state.searchFilterOrder[index];
+    newOrder[index] = this.state.searchFilterOrder[0];
+    this.setState({ searchFilterOrder: newOrder });
+  }
+
+  toggleDropDown() {
+    this.setState(prevState => ({
+      dropdownOpen: !prevState.dropdownOpen,
+    }));
   }
 
   updateSearchBarText(event) {
@@ -26,32 +56,47 @@ class Header extends React.Component {
     });
   }
 
-  renderButton() {
+  renderSearchBar() {
     return (
-      <Button onClick={() => { this.props.history.push(`/search/${this.state.searchbarText}`); }}>Search</Button>
+      <Form inline>
+        <Input id="searchBar" type="search" className="ml-4" placeholder="Search Dataset" onChange={(event) => { this.updateSearchBarText(event); }} />
+        <InputGroupButtonDropdown addonType="append" isOpen={this.state.dropdownOpen} toggle={this.toggleDropDown}>
+          <DropdownToggle outline caret>
+            {this.state.searchFilterOrder[0]}
+          </DropdownToggle>
+          <DropdownMenu>
+            <DropdownItem onClick={() => { this.switchFilter(1); }}>
+              {this.state.searchFilterOrder[1]}
+            </DropdownItem>
+            <DropdownItem divider />
+            <DropdownItem onClick={() => { this.switchFilter(2); }}>
+              {this.state.searchFilterOrder[2]}
+            </DropdownItem>
+          </DropdownMenu>
+          <Button color="primary" onClick={() => { this.props.history.push(`/search/${this.state.searchbarText}`); }}>Search</Button>
+        </InputGroupButtonDropdown>
+      </Form>
     );
   }
 
   render() {
+    const horizontalLineStyling = {
+      borderBottomColor: 'black',
+      borderBottomWidth: 1,
+      width: '100%',
+    };
+
     return (
       <div>
         <Row>
           <div className="d-inline-flex">
             <img id="logo" alt="DnDlogo" width="90" height="70" src={Logo} />
             <h1 id="header" onClick={() => { this.props.history.push('/'); }}>DataMaster</h1>
-            <Form inline>
-              <Input id="searchBar" type="search" className="ml-4" placeholder="Search Dataset" onChange={(event) => {this.updateSearchBarText(event)}} />
-              {this.renderButton()}
-            </Form>
+            {this.renderSearchBar()}
           </div>
         </Row>
         <Row>
-          <hr style={{
-            borderBottomColor: 'black',
-            borderBottomWidth: 1,
-            width: '100%',
-          }}
-          />
+          <hr style={horizontalLineStyling} />
         </Row>
       </div>
     );
