5bfd36efc0b721eadccfeb8e4be3c4300fff75bb
stbradle <sebradley13@gmail.com>
2019-01-17 17:15:59 -0800

    Fixed issues raised in PR #12 and updated style to adhere with airbnb

---
+++ dmsite/venv/src/App.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import 'filepond/dist/filepond.min.css';
+import DataMaster from './Datamaster';
+import './App.css';
+import BaseRouter from './routes';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <DataMaster>
+            <BaseRouter />
+          </DataMaster>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
