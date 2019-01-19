66d2c5d08726fd2b6516c97e0cd623d466f8c564
stbradle <sebradley13@gmail.com>
2019-01-18 21:29:49 -0800

    Added a cancel button for uploading files and made it so that it doesn't redirect when showing filepond

--- dmsite/venv/src/DataColumn/FilterButton.js
+++ dmsite/venv/src/DataColumn/FilterButton.js
@@ -1,31 +1,37 @@
 import React, { Component } from 'react';
 import 'filepond/dist/filepond.min.css';
 import { Input, Button } from 'reactstrap';
-import { Route } from "react-router-dom";
-import Upload from './UploadComponent'
-import { withRouter } from "react-router-dom";
+import { Route } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
+import Upload from './UploadComponent';
 
 class FilterButton extends Component {
-    constructor(props){
-        super(props);
-        this.uploadPressed = this.uploadPressed.bind(this)
-    }
-    uploadPressed(){
-        let path = '/home/upload'
-        this.props.history.push(path)
-    }
+  constructor(props) {
+    super(props);
+    this.uploadPressed = this.uploadPressed.bind(this);
+    this.state = {
+      uploading: false,
+    };
+  }
 
-render() {
-return (
-        <div>
-            <Input placeholder="Search Result" />
-            <div className="d-flex justify-content-center pt-2">
-                <Button color="primary" size="md" className="mr-1 btn-block mt-0" onClick={this.uploadPressed}>Add File</Button>
-                <Button color="primary"size="md" className="btn-block mt-0">Classify Me</Button>
-            </div>
-            <Route path='/home/upload' component={Upload} />
+  uploadPressed = (bool) => {
+    this.setState({
+      uploading: bool,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Input placeholder="Search Result" />
+        <div className="d-flex justify-content-center pt-2">
+          <Button block="false" color="primary" size="md" className="mr-1 btn-block mt-0" onClick={this.uploadPressed.bind(null, true)}>Add File</Button>
+          <Button color="primary" size="md" className="btn-block mt-0">Classify Me</Button>
         </div>
-    )
-   }
+        {this.state.uploading && <Upload /> }
+        {this.state.uploading && <Button close onClick={this.uploadPressed.bind(null, false)} />}
+      </div>
+    );
+  }
 }
 export default withRouter(FilterButton);
