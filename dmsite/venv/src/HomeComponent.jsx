07ab811a674c414066a2ff7d738844be2b3d2fd1
stbradle <stbradle@calpoly.edu>
2019-01-19 21:10:22 -0800

    Commented out code to allow dropdown selection of campaign

--- dmsite/venv/src/HomeComponent.jsx
+++ dmsite/venv/src/HomeComponent.jsx
@@ -1,15 +1,47 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
-import { Container, Row, Col } from 'reactstrap';
+import {
+  Container,
+  Row,
+  Col,
+  DropdownItem, Dropdown, DropdownMenu, DropdownToggle,
+} from 'reactstrap';
 import DatasetsColumn from './DataColumn/DatasetsColumn';
 import DisplayColumn from './VisualColumn/DisplayColumn';
 import ClassificationColumn from './ClassificationColumn/ClassificationsColumn';
 
 class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.toggleCampaignDropdown = this.toggleCampaignDropdown.bind(this);
+    this.state = {
+      campaignDropdownOpen: false,
+      selectedCampaign: null,
+    };
+  }
+
+  toggleCampaignDropdown() {
+    this.setState(prevState => ({
+      campaignDropdownOpen: !prevState.campaignDropdownOpen,
+    }));
+  }
+
   render() {
+    const { campaignDropdownOpen, selectedCampaign } = this.state;
+
     return (
       <Container fluid>
-        <h1>Campaign: Current Campaign</h1>
+        <Row>
+          <h1>Campaign: Current Campaign </h1>
+          {/*<Dropdown isOpen={campaignDropdownOpen} toggle={this.toggleCampaignDropdown}>*/}
+            {/*<DropdownToggle caret>*/}
+              {/*Select Campaign*/}
+            {/*</DropdownToggle>*/}
+            {/*<DropdownMenu>*/}
+              {/*<DropdownItem onClick={this.setCurrentCampaign}>Campaign 1</DropdownItem>*/}
+            {/*</DropdownMenu>*/}
+          {/*</Dropdown>*/}
+        </Row>
         <p> This campaign organizes Data1, Data2, Data3 </p>
         <Row>
           <Col md="3">
@@ -26,6 +58,6 @@ class Home extends React.Component {
       </Container>
     );
   }
-};
+}
 
 export default withRouter(Home);
