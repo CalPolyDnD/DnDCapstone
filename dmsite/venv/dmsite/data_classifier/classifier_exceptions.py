698b6dd4bcfa8ba316085cafe0a5b02cc827f618
Steven Bradley <stbradle@calpoly.edu>
2019-02-05 19:11:12 -0800

    Cassification (#19)
    * Added a working classifier.
    
    * Added pytest config file. It ignores some warnings that are thrown by our dependencies and searches for tests only in the directory dmsite/venv/dmsite/tests

--- dmsite/venv/dmsite/data_classifier/classifier_exceptions.py
+++ dmsite/venv/dmsite/data_classifier/classifier_exceptions.py
@@ -5,7 +5,7 @@ class Error(Exception):
 class UnsupportedFileTypeError(Error):
     def __init__(self, file_type):
         self.file_type = file_type
-        self.message = "File type '{}' not supported.".format(file_type)
+        self.message = f"File type '{file_type}' not supported."
 
     def __init__(self, file_type, message):
         self.file_type = file_type
