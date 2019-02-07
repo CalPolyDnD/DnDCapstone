c73967309269ce7807270eba0d5a13f167cd85aa
stbradle <sebradley13@gmail.com>
2019-02-07 15:46:42 -0800

    Repo reorganization

---
+++ dmsite/backend/dmsite/db_manager/test_db_manager.py
@@ -0,0 +1,83 @@
+import dmsite.db_manager.db_manager as dbm
+import pandas as pd
+from botocore.exceptions import ClientError
+
+testUserID = 'testUser'
+
+
+def test_dataset_conversion():
+    df_expected = pd.DataFrame({
+        "COUNT AMERICAN INDIAN": {
+          "0": 0,
+          "1": 0,
+          "2": 0,
+          "3": 0,
+          "4": 0,
+          "5": 0,
+          "6": 0,
+          "7": 0,
+          "8": 0,
+          "9": 0
+        },
+        "PERCENT AMERICAN INDIAN": {
+          "0": 0.5,
+          "1": 0.5,
+          "2": 0.5,
+          "3": 0.5,
+          "4": 0.3,
+          "5": 0.3,
+          "6": 0.3,
+          "7": 0.1,
+          "8": 0.1,
+          "9": 0.1
+        }
+    })
+
+    df_actual = pd.DataFrame(dbm._convert_to_dynamodb_types(df_expected)).apply(pd.to_numeric, errors='ignore')
+
+    assert pd.DataFrame.equals(df_expected, df_actual)
+
+
+def test_dataset_header_table_exists():
+    try:
+        dbm.dynamodb_client.describe_table(TableName=dbm.get_headers_table_name())
+        assert True
+    except dbm.dynamodb_client.exceptions.ResourceNotFoundException:
+        assert False
+
+
+def test_get_dataset_header():
+    actual = dbm.get_dataset_header('testUser', 'testDataset')
+
+    expected = {
+        "Table1": [{"id": 0, "item": "item 0"},
+                   {"id": 1, "item": "item 1"}]
+    }
+    assert actual == expected
+
+
+def test_put_dataset_header():
+    try:
+        dbm.get_headers_table().delete_item(
+            Key={
+                'UserID': testUserID,
+                'DatasetID': 'testInsertItem'
+            }
+        )
+    except ClientError:
+        raise
+
+    data = pd.DataFrame({
+        "Table1": [{"id": 0, "item": "item 0"},
+                   {"id": 1, "item": "item 1"}]
+    })
+
+    dbm.put_dataset_header(testUserID, 'testInsertItem', data)
+
+    result = dbm.get_headers_table().get_item(
+        Key={
+            'UserID': testUserID,
+            'DatasetID': 'testInsertItem'
+        }
+    )
+    assert result.get('Item')
---
+++ dmsite/frontend/src/Authentication/NewAccountTextField.jsx
@@ -0,0 +1,206 @@
+/**
+ * Created by christinadaley on 1/21/19.
+ */
+
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+import {
+  Button,
+  Form,
+  Icon,
+  Input,
+  Spin,
+} from 'antd';
+import { connect } from 'react-redux';
+import * as actions from '../store/actions/auth';
+
+/* const { Option } = Select;
+const AutoCompleteOption = AutoComplete.Option; */
+
+const antSpinner = <Icon type="loading" style={{ fontSize: 24 }} spin />;
+class NewAccountTextField extends React.Component {
+    state = {
+      confirmDirty: false,
+    };
+
+    handleSubmit = (e) => {
+      e.preventDefault();
+      const { form, onAuth } = this.props;
+      const { validateFieldsAndScroll } = form;
+      validateFieldsAndScroll((err, values) => {
+        if (!err) {
+          onAuth(values.email, values.password, values.confirm);
+          console.log('Received values of form: ', values);
+        }
+      });
+    }
+
+    handleConfirmBlur = (e) => {
+      const { value } = e.target;
+      this.setState((prevState) => {
+        const { confirmDirty } = this.state;
+        return {
+          ...prevState,
+          confirmDirty: confirmDirty || !!value,
+        };
+      });
+    }
+
+    compareToFirstPassword = (rule, value, callback) => {
+      const { form } = this.props;
+      if (value && value !== form.getFieldValue('password')) {
+        callback('Two passwords that you enter is inconsistent!');
+      } else {
+        callback();
+      }
+    }
+
+    validateToNextPassword = (rule, value, callback) => {
+      const { form } = this.props;
+      const { confirmDirty } = this.state;
+
+      if (value && confirmDirty) {
+        form.validateFields(['confirm'], { force: true });
+      }
+      callback();
+    }
+
+    /* handleWebsiteChange = (value) => {
+      let autoCompleteResult;
+      if (!value) {
+        autoCompleteResult = [];
+      } else {
+        autoCompleteResult = ['.com', '.org', '.net'].map(domain => `${value}${domain}`);
+      }
+      this.setState({ autoCompleteResult });
+    } */
+
+    render() {
+      const { form, loading, error } = this.props;
+      const { getFieldDecorator } = form;
+
+      let errorMessage = null;
+
+      if (error) {
+        errorMessage = (
+          <p>{error.message}</p>
+        );
+      }
+      /* const { autoCompleteResult } = this.state; */
+      /* const prefixSelector = getFieldDecorator('prefix', {
+        initialValue: '86',
+        })(
+          <Select style={{ width: 70 }}>
+           <Option value="86">+86</Option>
+           <Option value="87">+87</Option>
+          </Select>,
+       );
+
+      const websiteOptions = autoCompleteResult.map(website => (
+        <AutoCompleteOption key={website}>{website}</AutoCompleteOption>
+      )); */
+
+      return (
+        <Form onSubmit={this.handleSubmit}>
+          <Form.Item>
+            {getFieldDecorator('email', {
+              rules: [{ required: true, message: 'Please input your email!' }],
+            })(
+              <Input
+                style={{ backgroundColor: '#303030', borderWidth: 0 }}
+                placeholder="Email"
+              />,
+            )}
+          </Form.Item>
+          <Form.Item>
+            {getFieldDecorator('password', {
+              rules: [
+                { required: true, message: 'Please input your Password!' },
+                { validator: this.validateToNextPassword },
+              ],
+            })(
+              <Input
+                type="password"
+                placeholder="Password"
+                style={{ backgroundColor: '#303030', borderWidth: 0 }}
+              />,
+            )}
+          </Form.Item>
+          <Form.Item>
+            {getFieldDecorator('confirm', {
+              rules: [
+                { required: true, message: 'Passwords do not match!'},
+                { validator: this.compareToFirstPassword },
+              ],
+            })(
+              <Input
+                style={{ backgroundColor: '#303030', borderWidth: 0 }}
+                type="password"
+                placeholder="Confirm Password"
+                onBlur={this.handleConfirmBlur}
+              />,
+            )}
+          </Form.Item>
+          { errorMessage }
+          <Form.Item>
+            {
+              loading
+                ? <Spin indicator={antSpinner} />
+
+                : (
+                  <Button
+                    type="primary"
+                    htmlType="submit"
+                    className="btn-block mt0 login-form-button"
+                  >
+                    Create Account
+                  </Button>
+                )
+            }
+          </Form.Item>
+          { /* <Form.Item
+                    {...formItemLayout}
+                    label='Captcha'
+                    extra='We must make sure that your are a human.'
+                >
+                    <Row gutter={8}>
+                        <Col span={12}>
+                            {getFieldDecorator('captcha', {
+                                rules: [{ required: true, message: 'Please input
+                                the captcha you got!' }],
+                            })(
+                                <Input />
+                            )}
+                        </Col>
+                        <Col span={12}>
+                            <Button>Get captcha</Button>
+                        </Col>
+                    </Row>
+                </Form.Item> */ }
+          { /* <Form.Item {...tailFormItemLayout}>
+                    {getFieldDecorator('agreement', {
+                        valuePropName: 'checked',
+                    })(
+                        <Checkbox><a href=''> I have read the agreement</a></Checkbox>
+                    )}
+                </Form.Item> */ }
+          { /* <Form.Item {...tailFormItemLayout}>
+                    <Button type='primary' htmlType='submit'>Create Account</Button>
+                </Form.Item> */ }
+        </Form>
+      );
+    }
+}
+
+const mapStateToProps = state => ({
+  loading: state.loading,
+  error: state.error,
+});
+
+const mapDispatchToProps = dispatch => ({
+  onAuth: (email, pw1, pw2) => dispatch(actions.authSignup(email, pw1, pw2)),
+})
+
+const WrappedRegistrationForm = Form.create({ name: 'register' })(NewAccountTextField);
+
+export default connect(mapStateToProps, mapDispatchToProps)(withRouter(WrappedRegistrationForm));
