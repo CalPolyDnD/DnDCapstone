ca6a865f3696ca7c613fc16dc016272e98f348cc
Liang Cheung <lzcheung@calpoly.edu>
2019-02-11 22:57:43 -0800

    move test case

--- dmsite/venv/__tests__/CampaignPage/Campaign.spec.js
+++ dmsite/venv/__tests__/CampaignPage/Campaign.spec.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import Campaign from '../Campaign';
+import Campaign from '../../src/CampaignPage/Campaign';
 
 it('renders correctly', () => {
   const tree = renderer
