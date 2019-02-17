efd522acff550502354ec413d2de560e4ff5de67
lzcheung <lzcheung@235x31.ad.calpoly.edu>
2019-02-16 17:38:51 -0800

    start rest controller

---
+++ dmsite/frontend/src/Rest.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'http://localhost:8000';
+axios.defaults.xsrfHeaderName = "X-CSRFTOKEN";
+axios.defaults.xsrfCookieName = "csrftoken";
+
+const request = (method, url, body, success, failure) => {
+  const config = {
+    url: url,
+    method: method,
+    data: body,
+    responseType: 'json',
+  };
+  axios.request(config).then((response) => {
+    success(response);
+  }).catch((err) => {
+    failure(err);
+  });
+}
