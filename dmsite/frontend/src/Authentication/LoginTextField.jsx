96417b90e4152de27275a5954d0892e3039e49d0
cedaley <cedaley@calpoly.edu>
2019-03-04 22:06:23 -0800

    Added classfy button and redirect. Changed some mispelling on authentication pages.

--- dmsite/frontend/src/Authentication/LoginTextField.jsx
+++ dmsite/frontend/src/Authentication/LoginTextField.jsx
@@ -93,7 +93,7 @@ class TextField extends React.Component {
                     htmlType="submit"
                     className="btn-block mt0 login-form-button"
                   >
-                    Log in
+                    Login
                   </Button>
                 )
             }
--- dmsite/frontend/src/Authentication/NewAccountTextField.jsx
+++ dmsite/frontend/src/Authentication/NewAccountTextField.jsx
@@ -48,7 +48,7 @@ class NewAccountTextField extends React.Component {
     compareToFirstPassword = (rule, value, callback) => {
       const { form } = this.props;
       if (value && value !== form.getFieldValue('password')) {
-        callback('Two passwords that you enter is inconsistent!');
+        callback('The two passwords you entered are inconsistent');
       } else {
         callback();
       }
