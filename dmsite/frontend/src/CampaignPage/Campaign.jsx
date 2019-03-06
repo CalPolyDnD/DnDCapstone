25a9817534112ca93c45f29d639137acc257eb06
cedaley <cedaley@calpoly.edu>
2019-03-05 20:16:17 -0800

    Added to pull correct campaigns from db. Testing to come.

--- dmsite/frontend/src/CampaignPage/Campaign.jsx
+++ dmsite/frontend/src/CampaignPage/Campaign.jsx
@@ -1,10 +1,55 @@
 import React from 'react';
 import {
-  ListGroup, ListGroupItem, Card, CardBody, Button, CardHeader,
+  ListGroup, ListGroupItem, Card, CardBody, Button, CardHeader, NavLink, NavItem,
 } from 'reactstrap';
 import './style.css';
 
-class Campaign extends React.Component {
+class Campaign extends React.component{
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      campaigns: [],
+    };
+  }
+
+  componentDidMount() {
+    const FETCH_URL = 'http://localhost:8000/getUser';
+    const FETCH_URL2 = 'http://localhost:8000/getCampaigns'
+    const user = data.json();
+    fetch(FETCH_URL, {
+      method: 'POST',
+      body: JSON({
+        user: "user",
+      }, )
+    }).then((data) => {
+      this.setState({ user: user });
+    });
+    fetch(FETCH_URL2, {
+      method: 'POST',
+      body: JSON.stringify([{campaigns: user + ".campaigns"}])
+    }).then((data) => {
+      const campaigns = data.json();
+      this.setState({ campaigns: campaigns });
+    });
+  }
+
+  getCampaignList(campaigns){
+    const jsonObj = [];
+    for (let count = 0; count < this.state.compaigns.length; count++) {
+      const result = this.state.campaigns[count];
+      jsonObj.push(
+          <ListGroupItem
+              onClick={() => { this.props.history.push('/home' + campaigns.name); }}
+              style={{ backgroundColor: '#3d3d3d', color: 'white' }}
+          >
+            {result.name}
+          </ListGroupItem>
+      );
+    }
+    return jsonObj;
+  }
+
   render() {
     return (
       <div id="div" style={{ paddingTop: '5%' }}>
@@ -12,11 +57,7 @@ class Campaign extends React.Component {
           <CardHeader tag="h3" align="center" style={{ backgroundColor: '#303030', color: 'white' }}>Campaigns</CardHeader>
           <CardBody style={{ backgroundColor: '#3d3d3d', borderWidth: 0 }}>
             <ListGroup align="center" className="campaign-list" flush>
-              <div style={{ backgroundColor: '#303030' }}>
-                <ListGroupItem onClick={() => { this.props.history.push('/home'); }} style={{ backgroundColor: '#3d3d3d', color: 'white' }}>Campaign 1</ListGroupItem>
-                <ListGroupItem onClick={() => { this.props.history.push('/home'); }} style={{ backgroundColor: '#3d3d3d', color: 'white' }}>Campaign 2</ListGroupItem>
-                <ListGroupItem onClick={() => { this.props.history.push('/home'); }} style={{ backgroundColor: '#3d3d3d', color: 'white' }}>Campaign 3</ListGroupItem>
-              </div>
+              {this.getCampaignList()}
             </ListGroup>
             <Button color="primary" size="md" className="btn-block mt-3">Create New Campaign</Button>
           </CardBody>
