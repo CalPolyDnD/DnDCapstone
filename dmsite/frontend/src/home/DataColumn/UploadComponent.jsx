68bb9c0f55e8866e3793c9267cede239e35f8417
Griffin Aswegan <31191529+gdaswegan@users.noreply.github.com>
2019-04-16 10:37:38 -0700

    Update UploadComponent.jsx

--- dmsite/frontend/src/home/DataColumn/UploadComponent.jsx
+++ dmsite/frontend/src/home/DataColumn/UploadComponent.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { FilePond } from 'react-filepond';
 import 'filepond/dist/filepond.min.css';
 
+const UPLOAD_URL = 'http://localhost:8000/home/upload/push_file';
+
 function Upload(props){
   return (
     <div>
@@ -9,7 +11,7 @@ function Upload(props){
         allowMultiple
         style={{ backgroundColor: 'black', color: 'white' }}
         server={{
-          url: 'http://localhost:8000/home/upload/push_file',
+          url: UPLOAD_URL,
           process: {
             headers: {
               campaign: props.campaignName,
