50cfe1c86b7056adbcf152cfdf2a942788dd11ed
Griffin Aswegan <31191529+gdaswegan@users.noreply.github.com>
2019-04-30 13:33:16 -0700

    Updated run.sh to keep you in the same directory

--- dmsite/run.sh
+++ dmsite/run.sh
@@ -2,7 +2,8 @@ cd backend
 if [ -n `pip3 freeze | grep -q -e virtualenv` ]; then
    echo "Virtual Environment not active."
    echo "Activating Virtual Environment - please rerun this script if the server does not start."
-   source env/bin/activate
+   cd ..
+   source backend/env/bin/activate
 fi
 mkdir -p media
 python3 manage.py runserver
