fdddd065398d160907ada7fd48d94cbc7260f88e
Dustyn Zierman-Felix <dziermanfelix@gmail.com>
2019-05-16 08:45:03 -0700

    Adding request code

--- dmsite/frontend/src/Vis/Vis.jsx
+++ dmsite/frontend/src/Vis/Vis.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
-import Graph from "react-graph-vis";
+import Graph from 'react-graph-vis';
 import { render } from "react-dom";
+import axios from 'axios';
+const FETCH_URL = 'http://localhost:8000/get_files';
+const FETCH_CURRENT_USER_URL = 'http://localhost:8000/rest-auth/user/';
 
 class Vis extends React.Component {
     constructor(props) {
@@ -33,6 +36,20 @@ class Vis extends React.Component {
             var {nodes, edges} = event;
         }
     };
+
+    componentDidMount() {
+        axios.get(FETCH_CURRENT_USER_URL)
+           .then((userRes) => {
+             fetch(FETCH_URL, {
+               method: 'POST',
+               body: JSON.stringify({
+                 campaign: 'test_campaign',
+               }),
+             }).then(data => data.json()).then((response) => {
+             // put your code here
+           });
+         });
+    }
 
     render() {
       return (
